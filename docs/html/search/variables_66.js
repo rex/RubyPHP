b5adcdc36b84fe055c607d3950e1d50596f2acea
= <=>
2012-04-03 11:40:03 -0500

    Same version, but without all the printed text. That would be annoying.

--- docs/html/search/variables_66.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['flip',['flip',['../index_8php.html#a976e7b3d05fce3934ce5bc7a635bcade',1,'index.php']]]
-];
--- docs/html/search/variables_6c.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['length',['length',['../index_8php.html#a328bd7d697cc1b3dfd1859d35420f2d0',1,'index.php']]]
-];
