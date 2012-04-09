c5f54429e251a2cdf7c9f8c08b8badac82831a2b
Pierce Moore <Pierce@PierceMoore.com>
2012-04-09 14:34:03 -0500

    NOW I got the docs cleaned up.

--- docs/html/search/enums_63.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['chaining',['CHAINING',['../class_8rubyphp_8php.html#a2ef56c4551609dbec9cf98bd6008f646',1,'class.rubyphp.php']]]
-];
--- docs/html/search/enums_64.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['debug',['DEBUG',['../class_8rubyphp_8php.html#a73585d7121de037cf2e2ca12b27eb83e',1,'class.rubyphp.php']]]
-];
